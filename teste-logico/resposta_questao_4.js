864a9c077db198c90143e498ba32a113dc961ae9
Lucas Maranho <lksribeiro2014@gmail.com>
2024-03-13 20:23:28 -0300

    respostas questões 4 e 5

--- teste-logico/resposta_questao_4.js
+++ teste-logico/resposta_questao_4.js
@@ -1,1 +1,42 @@
 
+function checksPeopleWorking(arr) {
+  let anos = [];
+  let anosComMaisPessoas = [];
+  let aux = 0;
+
+  // verifica cada item do array de range de anos de cada pessoa 
+  arr.map( item => {
+    for(let i = item[0]; i <= item[1]; i++){
+
+      // cria um array com os anos trabalhados 
+      let ano = i;
+      anos.push(ano)
+    }
+  });
+
+  // verifica a quantidade de cada ano que se repete 
+  let pessoasPorAno = anos.reduce(function (obj, item) {
+    obj[item] = (obj[item] || 0) + 1;
+    return obj;
+  }, {});
+
+
+  // verifica maior quantidade de pessoas que trabalharam em um ano 
+  Object.keys(pessoasPorAno).forEach(function (item) {
+    if(pessoasPorAno[item] > aux){
+      aux = pessoasPorAno[item];
+    }
+  });
+
+  Object.keys(pessoasPorAno).forEach(function (item) {
+    if (pessoasPorAno[item] == aux) {
+      anosComMaisPessoas.push(item)
+    }
+  });
+  
+  console.log(`O(s) ano(s) que mais pessoa(s) trabalharam foi: \n\n ${anosComMaisPessoas} \n\n com total de ${aux} pessoas trabalhando em cada ano.`);
+}
+
+checksPeopleWorking([
+   [1945, 2008], [1938, 1999], [2000, 2021], [2000, 2005],[1960,2008]
+]);
--- teste-logico/resposta_questao_5.js
+++ teste-logico/resposta_questao_5.js
@@ -1,1 +1,20 @@
 
+function modosFotos(val) {
+  let aux = val;
+
+  // crio um contador em ordem decrescente até chegar em 1 para calcular o número fatorial 
+  for (let i = val; i > 0; i--) {
+
+    // verifico se o numero anterior é diferente de 0 
+    if (i - 1 !== 0) {
+
+      // pego o numero de pessoas e multiplico pelo número anterior 
+      aux = aux * (i - 1);
+    }
+  }
+
+  console.log(`É possível tirar a foto de ${aux} modos distintos.`);
+}
+
+let numeroPessoas = 6;
+modosFotos(numeroPessoas);
